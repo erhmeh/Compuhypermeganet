5332de11d5973ecbb020c6526081013e0e43b96d
Unknown <pdnrobertson@gmail.com>
2018-07-12 14:25:02 +0300

    Auto stash before merge of "master" and "origin/master"

--- Main Project/lib/quadruped/quadruped.h
+++ Main Project/lib/quadruped/quadruped.h
@@ -10,14 +10,18 @@ class quadruped {
         void setPins(int pins[]);
         void attachServos();
         /* Movement functions */
-        void standUp();
-        void lowerDown();
-        void moveForward(int steps);
-        void moveBackward(int steps);
+        void standUp(); 
+        void lowerDown(); 
+        void moveForward(int steps); 
+        void moveBackward(int steps); 
     private:
         /* 0-1=top-left, 2-3=top-right, 4-5=bottom-left, 6-7=bottom-right */
         Servo servos[8]; 
         int pins_[8];
+        /* dimensions of leg components */
+        const float femur = 80.0;
+        const float tibia = 100.0;
+        const float coxa = 20.0; /* placeholder value, could be changed */
 };
 
 #endif
--- Main Project/src/main.cpp
+++ Main Project/src/main.cpp
@@ -7,6 +7,7 @@
 
 #include "Arduino.h"
 #include "adxl337.h"
+#include "quadruped.h"
 
 // Remove the Serial writing for faster processing
 #define DEBUG_PRINT(x)      Serial.print(x)
@@ -16,11 +17,23 @@
 #define DEBUG_PRINTLNHEX(x) Serial.println(x, HEX)
 
 // IO pins
+/* Accelerometer Pins */
 #define ADXL337_X_PIN 12
 #define ADXL337_Y_PIN 13
 #define ADXL337_Z_PIN 14
 
+/* Servo Pins */
+#define L4_1 0
+#define L4_2 1
+#define L3_1 2
+#define L3_2 3
+#define L2_1 4
+#define L2_2 5
+#define L1_1 6
+#define L1_2 7
+
 adxl337 accel; // Accelerometer object
+quadruped quad; // Quadruped object
 
 /* Parse this array to the the accelerometer class to fill it with most recent
    acceleration readings
@@ -33,6 +46,12 @@ float recentAccel[3];
 void initADXL337()
 {
     accel.setPins(ADXL337_X_PIN, ADXL337_Y_PIN, ADXL337_Z_PIN);
+}
+
+void initQuadruped() {
+    int pins[] = {L4_1, L4_2, L3_1, L3_2, L2_1, L2_2, L1_1, L1_2};
+    quad.setPins(pins);
+    quad.attachServos();
 }
 
 // main setup
