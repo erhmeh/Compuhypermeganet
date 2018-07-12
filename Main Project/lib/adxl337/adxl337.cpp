8d7d30802f00190fd99e0b73bf5904cbf336b18c
Jamin <jaminearly@gmail.com>
2018-07-12 13:58:04 +0300

    Added comments

--- Main Project/lib/adxl337/adxl337.cpp
+++ Main Project/lib/adxl337/adxl337.cpp
@@ -40,12 +40,15 @@ float adxl337::mapf(float x, float in_min, float in_max, float out_min, float ou
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+// Calculations from http://physics.rutgers.edu/~aatish/teach/srr/workshop3.pdf 
+// Parse the accelerometer array and reuturn the pitch in degrees
 float adxl337::getPitch(float * accel){
     float pitch = atan(accel[0] / sqrt(pow(accel[1], 2) + pow(accel[2], 2)));
     pitch = pitch * (180.0 / PI);
     return pitch;
 }
 
+// Parse the accelerometer array and return the roll in degrees
 float adxl337::getRoll(float * accel){
     float roll = atan(accel[1] / sqrt(pow(accel[0], 2) + pow(accel[2], 2)));
     roll = roll * (180.0 / PI);
